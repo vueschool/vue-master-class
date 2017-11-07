32011b2c13a55c19b6957e7f3577227ca401303a
Alex Kyriakidis <hootlex@icloud.com>
2017-11-07 20:01:57 +0200

    install theme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -12,12 +14,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/css/style.css";
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    <h1>Welcome to the forum</h1>
-    <div v-for="thread in threads">
-      <h2>{{thread.title}}</h2>
-      <div v-for="postId in thread.posts">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
+    <div v-for="thread in threads"
+         class="col-large push-top"
+    >
+      <h1>{{thread.title}}</h1>
+
+      <div class="post-list">
+        <div v-for="postId in thread.posts"
+             class="post"
+        >
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>
+              {{posts[postId].text}}
+            </div>
+          </div>
+
+          <div class="post-date text-faded">
+            {{posts[postId].publishedAt}}
+          </div>
+        </div>
       </div>
     </div>
   </div>
