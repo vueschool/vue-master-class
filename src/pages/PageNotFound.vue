c0fae2e433015a7355578a4096416f45b3f86422
Alex Kyriakidis <hootlex@icloud.com>
2017-11-09 23:25:43 +0200

    handling not found pages

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="col-full">
+      <h1>Not Found</h1>
+      <p>Ooops, we couldn't find what you are looking for. Why don't you
+        <router-link :to="{name: 'Home'}">
+          go home instead?
+        </router-link>
+      </p>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 100px;
+  }
+  p {
+    font-size: 50px;
+    font-weight: 100;
+  }
+  div {
+    text-align: center;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
+import NotFound from '@/pages/PageNotFound'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +17,11 @@ export default new Router({
       name: 'ThreadShow',
       component: ThreadShow,
       props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ],
   mode: 'history'
