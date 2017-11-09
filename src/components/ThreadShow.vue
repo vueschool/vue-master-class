4d924023e8ece7b6b6bb93bbe0c73c1057027762
Alex Kyriakidis <hootlex@icloud.com>
2017-11-09 16:24:38 +0200

    create thread page

---
+++ src/components/ThreadShow.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+
+    <div class="post-list">
+      <div v-for="postId in thread.posts"
+           class="post"
+      >
+        <div class="user-info">
+          <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+          <a href="#">
+            <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            {{posts[postId].text}}
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{posts[postId].publishedAt}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data'
+  export default {
+    props: {
+      id: {
+        required: true,
+        type: String
+      }
+    },
+
+    data () {
+      return {
+        thread: sourceData.threads[this.id],
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import ThreadShow from '@/components/ThreadShow'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,13 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
+      props: true
     }
-  ]
+  ],
+  mode: 'history'
 })
