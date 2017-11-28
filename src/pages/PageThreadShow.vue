fe2d3bda6a85ad4955ad6d2cd1f0d0a2b70586da
Alex Kyriakidis <hootlex@icloud.com>
2017-11-28 21:04:23 +0200

    adding new posts

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,21 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          class="form-input"
+          v-model="newPostText"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -22,7 +37,8 @@
 
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
       }
     },
 
@@ -31,6 +47,22 @@
         const postIds = Object.values(this.thread.posts)
         return Object.values(sourceData.posts)
           .filter(post => postIds.includes(post['.key']))
+      }
+    },
+    methods: {
+      addPost () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+          '.key': postId
+        }
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''
       }
     }
   }
