09608cf38dbbb171bf980879e2f64ca101e937f8
Alex Kyriakidis <hootlex@icloud.com>
2017-12-09 01:01:50 +0200

    create the Forum page

---
+++ src/pages/PageForum.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="forum-wrapper">
+    <div class="col-full push-top">
+      <div class="forum-header">
+        <div class="forum-details">
+          <h1>{{forum.name}}</h1>
+          <p class="text-lead">{{forum.description}}</p>
+        </div>
+        <a href="new-thread.html" class="btn-green btn-small">Start a thread</a>
+      </div>
+    </div>
+
+    <div class="col-full push-top">
+      <ThreadList :threads="threads"/>
+    </div>
+  </div>
+</template>
+
+<script>
+    import ThreadList from '@/components/ThreadList'
+    import sourceData from '@/data'
+    export default {
+      components: {
+        ThreadList
+      },
+
+      props: {
+        id: {
+          required: true,
+          type: String
+        }
+      },
+
+      computed: {
+        forum () {
+          return sourceData.forums[this.id]
+        },
+
+        threads () {
+          return Object.values(sourceData.threads)
+            .filter(thread => thread.forumId === this.id)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .forum-wrapper {
+    width: 100%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
+import Forum from '@/pages/PageForum'
 import NotFound from '@/pages/PageNotFound'
 Vue.use(Router)
 
@@ -11,6 +12,12 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/forum/:id',
+      name: 'Forum',
+      component: Forum,
+      props: true
     },
     {
       path: '/thread/:id',
