735b57546c9dd2f80c8179ca30e9d51708bd2e4e
Alex Kyriakidis <hootlex@icloud.com>
2018-02-22 21:39:04 +0100

    export ThreadEditor component

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -3,38 +3,24 @@
 
     <h1>Create new thread in <i>{{forum.name}}</i></h1>
 
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input v-model="title" type="text" id="thread_title" class="form-input" name="title">
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea v-model="text" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-      </div>
-    </form>
+    <ThreadEditor
+      @save="save"
+      @cancel="cancel"
+    />
   </div>
 </template>
 
 <script>
+    import ThreadEditor from '@/components/ThreadEditor'
     export default {
+      components: {
+        ThreadEditor
+      },
+
       props: {
         forumId: {
           type: String,
           required: true
-        }
-      },
-
-      data () {
-        return {
-          title: '',
-          text: ''
         }
       },
 
@@ -45,11 +31,11 @@
       },
 
       methods: {
-        save () {
+        save ({title, text}) {
           this.$store.dispatch('createThread', {
             forumId: this.forum['.key'],
-            title: this.title,
-            text: this.text
+            title,
+            text
           }).then(thread => {
             this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
           })
