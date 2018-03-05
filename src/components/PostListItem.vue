11c46dc7fa79302657717a2551deca9b37c0dae9
Alex Kyriakidis <hootlex@icloud.com>
2018-03-05 12:02:36 +0100

    add edited indicator

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -28,6 +28,7 @@
     </div>
 
     <div class="post-date text-faded">
+      <div v-if="post.edited" class="edition-info">edited</div>
       <AppDate :timestamp="post.publishedAt"/>
     </div>
   </div>
--- src/store/index.js
+++ src/store/index.js
@@ -49,25 +49,33 @@ export default new Vuex.Store({
       })
     },
 
-    updateThread ({state, commit}, {title, text, id}) {
+    updateThread ({state, commit, dispatch}, {title, text, id}) {
       return new Promise((resolve, reject) => {
         const thread = state.threads[id]
-        const post = state.posts[thread.firstPostId]
-
         const newThread = {...thread, title}
-        const newPost = {...post, text}
-
         commit('setThread', {thread: newThread, threadId: id})
-        commit('setPost', {post: newPost, postId: thread.firstPostId})
 
-        resolve(newThread)
+        dispatch('updatePost', {id: thread.firstPostId, text})
+          .then(() => {
+            resolve(newThread)
+          })
       })
     },
 
     updatePost ({state, commit}, {id, text}) {
       return new Promise((resolve, reject) => {
         const post = state.posts[id]
-        commit('setPost', {postId: id, post: {...post, text}})
+        commit('setPost', {
+          postId: id,
+          post: {
+            ...post,
+            text,
+            edited: {
+              at: Math.floor(Date.now() / 1000),
+              by: state.authId
+            }
+          }
+        })
         resolve(post)
       })
     },
