6c22eacbe09a21ad1bfe663be9f8b2424214f509
Alex Kyriakidis <hootlex@icloud.com>
2018-04-04 16:33:29 +0200

    install firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -9,6 +10,17 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyC_WLhTuqbKU3unweXosx8zsKQccBOKs7c',
+  authDomain: 'vue-school-forum.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum.firebaseio.com',
+  projectId: 'vue-school-forum',
+  storageBucket: 'vue-school-forum.appspot.com',
+  messagingSenderId: '426987850952'
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
