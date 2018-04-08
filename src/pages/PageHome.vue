1f7aa4faa5e3197453a92ecd2c3a101944997f73
Alex Kyriakidis <hootlex@icloud.com>
2018-04-08 17:49:33 +0200

    fetch categories and forums for the home page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -17,6 +17,13 @@ export default {
     categories () {
       return Object.values(this.$store.state.categories)
     }
+  },
+
+  beforeCreate () {
+    this.$store.dispatch('fetchAllCategories')
+      .then(categories => {
+        categories.forEach(category => this.$store.dispatch('fetchForums', {ids: Object.keys(category.forums)}))
+      })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -115,8 +115,26 @@ export default new Vuex.Store({
       return dispatch('fetchItem', {resource: 'posts', id, emoji: '💬'})
     },
 
+    fetchForums ({dispatch}, {ids}) {
+      return dispatch('fetchItems', {resource: 'forums', emoji: '🌧', ids})
+    },
+
     fetchPosts ({dispatch}, {ids}) {
       return dispatch('fetchItems', {resource: 'posts', emoji: '💬', ids})
+    },
+
+    fetchAllCategories ({state, commit}) {
+      console.log('🔥', '🏷', 'all')
+      return new Promise((resolve, reject) => {
+        firebase.database().ref('categories').once('value', snapshot => {
+          const categoriesObject = snapshot.val()
+          Object.keys(categoriesObject).forEach(categoryId => {
+            const category = categoriesObject[categoryId]
+            commit('setItem', {resource: 'categories', id: categoryId, item: category})
+          })
+          resolve(Object.values(state.categories))
+        })
+      })
     },
 
     fetchItem ({state, commit}, {id, emoji, resource}) {
