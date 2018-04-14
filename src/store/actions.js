1947f36f8eebd08f96137e644c068a53c8f4d8c9
Alex Kyriakidis <hootlex@icloud.com>
2018-04-14 19:10:38 +0200

    store posts in firebase

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,15 +2,21 @@ import firebase from 'firebase'
 
 export default {
   createPost ({commit, state}, post) {
-    const postId = 'greatPost' + Math.random()
-    post['.key'] = postId
+    const postId = firebase.database().ref('posts').push().key
     post.userId = state.authId
     post.publishedAt = Math.floor(Date.now() / 1000)
 
-    commit('setPost', {post, postId})
-    commit('appendPostToThread', {parentId: post.threadId, childId: postId})
-    commit('appendPostToUser', {parentId: post.userId, childId: postId})
-    return Promise.resolve(state.posts[postId])
+    const updates = {}
+    updates[`posts/${postId}`] = post
+    updates[`threads/${post.threadId}/posts/${postId}`] = postId
+    updates[`users/${post.userId}/posts/${postId}`] = postId
+    firebase.database().ref().update(updates)
+      .then(() => {
+        commit('setItem', {resource: 'posts', item: post, id: postId})
+        commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+        commit('appendPostToUser', {parentId: post.userId, childId: postId})
+        return Promise.resolve(state.posts[postId])
+      })
   },
 
   createThread ({state, commit, dispatch}, {text, title, forumId}) {
