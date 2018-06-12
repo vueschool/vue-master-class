5d60780036873513acf704011027f710105043ef
Alex Kyriakidis <hootlex@icloud.com>
2018-06-12 11:00:26 +0200

    dropdown menu using class binding

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -19,26 +19,27 @@
     <nav class="navbar">
       <ul v-if="user">
         <li class="navbar-user">
-          <router-link :to="{name: 'Profile'}">
+          <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="user.avatar" alt="">
             <span>
                 {{user.name}}
                 <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
             </span>
-          </router-link>
+          </a>
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
-          <div id="user-dropdown">
+          <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
-              <li class="dropdown-menu-item"><a href="profile.html">View profile</a></li>
-              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+              <li class="dropdown-menu-item">
+                <router-link :to="{name: 'Profile'}">View Profile</router-link>
+              </li>
+              <li class="dropdown-menu-item">
+                <a @click.prevent="$store.dispatch('signOut')">Sign Out</a>
+              </li>
             </ul>
           </div>
-        </li>
-        <li class="navbar-item">
-          <a @click.prevent="$store.dispatch('signOut')">Sign Out</a>
         </li>
       </ul>
       <ul v-else>
@@ -56,6 +57,12 @@
 <script>
   import {mapGetters} from 'vuex'
   export default {
+    data () {
+      return {
+        userDropdownOpen: false
+      }
+    },
+
     computed: {
       ...mapGetters({
         'user': 'authUser'
