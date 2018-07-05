47bf8ad86089526e7a7ae5fbc9894ec0ce81282f
Alex Kyriakidis <hootlex@icloud.com>
2018-07-05 14:51:53 +0200

    add requiresGuest meta and finalise protected routes

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,8 @@ const router = new Router({
       path: '/thread/create/:forumId',
       name: 'ThreadCreate',
       component: ThreadCreate,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
     },
     {
       path: '/thread/:id',
@@ -48,7 +49,8 @@ const router = new Router({
       path: '/thread/:id/edit',
       name: 'ThreadEdit',
       component: ThreadEdit,
-      props: true
+      props: true,
+      meta: { requiresAuth: true }
     },
     {
       path: '/me',
@@ -61,21 +63,25 @@ const router = new Router({
       path: '/me/edit',
       name: 'ProfileEdit',
       component: Profile,
-      props: {edit: true}
+      props: {edit: true},
+      meta: { requiresAuth: true }
     },
     {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
+      meta: { requiresGuest: true }
     },
     {
       path: '/signin',
       name: 'SignIn',
-      component: SignIn
+      component: SignIn,
+      meta: { requiresGuest: true }
     },
     {
       path: '/logout',
       name: 'SignOut',
+      meta: { requiresAuth: true },
       beforeEnter (to, from, next) {
         store.dispatch('signOut')
           .then(() => next({name: 'Home'}))
@@ -98,6 +104,13 @@ router.beforeEach((to, from, next) => {
       if (to.matched.some(route => route.meta.requiresAuth)) {
         // protected route
         if (user) {
+          next()
+        } else {
+          next({name: 'SignIn'})
+        }
+      } else if (to.matched.some(route => route.meta.requiresGuest)) {
+        // protected route
+        if (!user) {
           next()
         } else {
           next({name: 'Home'})
