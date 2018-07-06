a44d40bfc0598fdefbeee3421d5f84e544438c76
Alex Kyriakidis <hootlex@icloud.com>
2018-07-06 21:45:29 +0200

    auth user profile page

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="flex-grid">
-    <h1>My Profile</h1>
-    <!--<UserProfileCard-->
-      <!--v-if="!edit"-->
-      <!--:user="user"-->
-    <!--/>-->
-    <!--<UserProfileCardEditor-->
-      <!--v-else-->
-      <!--:user="user"-->
-    <!--/>-->
+    <UserProfileCard
+      v-if="!edit"
+      :user="user"
+    />
+    <UserProfileCardEditor
+      v-else
+      :user="user"
+    />
 
-    <!--<div class="col-7 push-top">-->
+    <div class="col-7 push-top">
+      <div class="profile-header">
+        <span class="text-lead">
+            {{user.username}}'s recent activity
+        </span>
+        <a href="#">See only started threads?</a>
+      </div>
 
-      <!--<div class="profile-header">-->
-        <!--<span class="text-lead">-->
-            <!--{{user.username}}'s recent activity-->
-        <!--</span>-->
-        <!--<a href="#">See only started threads?</a>-->
-      <!--</div>-->
-
-      <!--<hr>-->
-      <!--<PostList :posts="userPosts"/>-->
-    <!--</div>-->
+      <hr>
+      <PostList :posts="userPosts"/>
+    </div>
   </div>
 </template>
 
@@ -30,6 +28,7 @@
     import UserProfileCard from '@/components/UserProfileCard'
     import UserProfileCardEditor from '@/components/UserProfileCardEditor'
     import {mapGetters} from 'vuex'
+    import asyncDataStatus from '@/mixins/asyncDataStatus'
 
     export default {
       components: {
@@ -37,6 +36,8 @@
         UserProfileCard,
         UserProfileCardEditor
       },
+
+      mixins: [asyncDataStatus],
 
       props: {
         edit: {
@@ -51,15 +52,12 @@
         }),
 
         userPosts () {
-          if (this.user.posts) {
-            return Object.values(this.$store.state.posts)
-              .filter(post => post.userId === this.user['.key'])
-          }
-          return []
+          return this.$store.getters.userPosts(this.user['.key'])
         }
       },
       created () {
-        this.$emit('ready')
+        this.$store.dispatch('fetchPosts', {ids: this.user.posts})
+          .then(() => this.asyncDataStatus_fetched())
       }
     }
 </script>
--- src/store/getters.js
+++ src/store/getters.js
@@ -5,6 +5,15 @@ export default {
     return state.authId ? state.users[state.authId] : null
   },
 
+  userPosts: state => id => {
+    const user = state.users[id]
+    if (user.posts) {
+      return Object.values(state.posts)
+        .filter(post => post.userId === id)
+    }
+    return []
+  },
+
   userThreadsCount: state => id => countObjectProperties(state.users[id].threads),
   userPostsCount: state => id => countObjectProperties(state.users[id].posts),
   threadRepliesCount: state => id => countObjectProperties(state.threads[id].posts) - 1
