ade08aebca57c13d7d9820e2e813c200a9e9de8b
Alex Kyriakidis <hootlex@icloud.com>
2018-08-21 15:31:05 +0200

    validate that an input is an image url and it exists

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -56,11 +56,13 @@
         <div class="form-group">
           <label for="avatar">Avatar</label>
           <input
-            v-model="form.avatar"
+            v-model.lazy="form.avatar"
             @blur="$v.form.avatar.$touch()"
             id="avatar" type="text" class="form-input">
           <template v-if="$v.form.avatar.$error">
-
+            <span v-if="!$v.form.avatar.url" class="form-error">The supplied URL is invalid</span>
+            <span v-else-if="!$v.form.avatar.supportedImageFile" class="form-error">This file type is not supported by our system. Supported file types: .jpg, .png, .gif, .jpeg, .svg</span>
+            <span v-else-if="!$v.form.avatar.responseOk" class="form-error">The supplied image cannot be found</span>
           </template>
         </div>
 
@@ -78,7 +80,7 @@
 
 <script>
     import firebase from 'firebase'
-    import {required, email, minLength, helpers as vuelidateHelpers} from 'vuelidate/lib/validators'
+    import {required, email, minLength, url, helpers as vuelidateHelpers} from 'vuelidate/lib/validators'
     export default {
       data () {
         return {
@@ -127,7 +129,25 @@
             minLength: minLength(6)
           },
           avatar: {
-
+            url,
+            supportedImageFile (value) {
+              if (!vuelidateHelpers.req(value)) {
+                return true
+              }
+              const supported = ['jpg', 'jpeg', 'gif', 'png', 'svg']
+              const suffix = value.split('.').pop()
+              return supported.includes(suffix)
+            },
+            responseOk (value) {
+              if (!vuelidateHelpers.req(value)) {
+                return true
+              }
+              return new Promise((resolve, reject) => {
+                fetch(value)
+                  .then(response => resolve(response.ok))
+                  .catch(() => resolve(false))
+              })
+            }
           }
         }
       },
